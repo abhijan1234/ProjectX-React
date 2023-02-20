2e3b99d71a42d6d3c8754fd1c383f807f5ad6d1e
Abhijan.Bhattacharyya <Abhijan.Bhattacharyya@bentley.com>
2023-02-20 21:23:35 +0530

    Project Card with dummy data

--- src/Layouts/Projects/Projects.tsx
+++ src/Layouts/Projects/Projects.tsx
@@ -3,14 +3,15 @@ import { getAllProjects } from '../../API/ProjectAPIs'
 import Card from '../../Components/Card/Card'
 import Navbar from '../../Components/Navbar/Navbar'
 import Sidebar from '../../Components/Sidebar/Sidebar'
+import Data from "../../Testdata.json"
 import "./Projects.scss"
 const Projects = (props: any) => {
-  const [projectList,setProjectList] = useState([])
+  const [projectList,setProjectList] = useState([{"ProjectName":"","Status":"","StartTime":"","EndTime":"","Mentor":""}])
   useEffect(()=>{
     getAllProjectInformation()
   },[])
   const getAllProjectInformation = async()=>{
-    var data = await getAllProjects()
+    var data = Data//await getAllProjects()
     console.log("Data isss",data)
     setProjectList(data)
   } 
@@ -22,12 +23,47 @@ const Projects = (props: any) => {
         <div className='project-row-tile'>
           <div className='projectPageContainer'>
             <Card headerName = "My Projects">
-              <table>
-                <tr>
-                  <th style={{"paddingRight":"85px"}}>Project Name</th>
-                  <th style={{"paddingRight":"20px"}}>Status</th>
-                  <th>Start Time</th>
+              <table className='content-table'>
+                <thead>
+                  <tr>
+                    <th>Project Name</th>
+                    <th>Status</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Mentor</th>
+                  </tr>
+                </thead>
+                <tbody>{
+                  projectList.map(item=>
+                    <tr>
+                      <td>{item.ProjectName}</td>
+                      <td>{item.Status}</td>
+                      <td>{item.StartTime}</td>
+                      <td>{item.EndTime.length>0?item.EndTime:"-"}</td>
+                      <td>{item.Mentor}</td>
+                    </tr>
+                    )
+                  }
+                </tbody>
+                {/* <tr>
+                  <th style={{"paddingRight":"30px"}}>Project Name</th>
+                  <th style={{"paddingRight":"70px"}}>Status</th>
+                  <th style={{"paddingRight":"60px"}}>Start Time</th>
+                  <th style={{"paddingRight":"32px"}}>End Time</th>
+                  <th style={{"paddingRight":"10px"}}>Mentor</th>
                 </tr>
+                <tbody>{
+                  projectList.map(item=>
+                    <tr>
+                      <td style={{"paddingLeft":"35px"}}>{item.ProjectName}</td>
+                      <td style={{"paddingRight":"35px"}}>{item.Status}</td>
+                      <td style={{"paddingRight":"35px"}}>{item.StartTime}</td>
+                      <td style={{"textAlign": "center"}}>{item.EndTime.length>0?item.EndTime:"-"}</td>
+                      <td style={{"paddingLeft":"15px"}}>{item.Mentor}</td>
+                    </tr>
+                    )
+                  }
+                </tbody> */}
               </table>
             </Card>
           </div>
