f1db2916cb730f57be2da367ab4bdfa0125ea05b
Abhijan.Bhattacharyya <Abhijan.Bhattacharyya@bentley.com>
2023-02-21 21:53:48 +0530

    Sidebar style

--- src/Layouts/Projects/Projects.tsx
+++ src/Layouts/Projects/Projects.tsx
@@ -1,64 +1,61 @@
-import { useEffect, useState } from 'react'
-import { getAllProjects } from '../../API/ProjectAPIs'
-import Card from '../../Components/Card/Card'
-import Navbar from '../../Components/Navbar/Navbar'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import Data from "../../Testdata.json"
-import "./Projects.scss"
+import { useEffect, useState } from "react";
+import { getAllProjects } from "../../API/ProjectAPIs";
+import Card from "../../Components/Card/Card";
+import Navbar from "../../Components/Navbar/Navbar";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import Data from "../../Testdata.json";
+import "./Projects.scss";
 const Projects = (props: any) => {
-  const [projectList,setProjectList] = useState([{"ProjectName":"","Status":"","StartTime":"","EndTime":"","Mentor":""}])
-  useEffect(()=>{
-    getAllProjectInformation()
-  },[])
-  const getAllProjectInformation = async()=>{
-    var data = Data//await getAllProjects()
-    console.log("Data isss",data)
-    setProjectList(data)
-  } 
+  const [projectList, setProjectList] = useState([
+    { ProjectName: "", Status: "", StartTime: "", EndTime: "", Mentor: "" },
+  ]);
+  useEffect(() => {
+    getAllProjectInformation();
+  }, []);
+  const getAllProjectInformation = async () => {
+    var data = Data; //await getAllProjects()
+    console.log("Data isss", data);
+    setProjectList(data);
+  };
   return (
     <div>
       <Navbar />
       <Sidebar />
-      <div className='project-main-tile'>
-        <div className='project-row-tile'>
-          <div className='projectPageContainer'>
-            <div className='projectListCard'>
-              <Card headerName = "My Projects">
-                <table className='content-table'>
-                  <thead>
+      <div className="project-main-tile">
+        <div className="projectPageContainer">
+          <div className="projectListCard">
+            <Card headerName="My Projects">
+              <table className="content-table">
+                <thead>
+                  <tr>
+                    <th>Project Name</th>
+                    <th>Status</th>
+                    <th>Start Time</th>
+                    <th>End Time</th>
+                    <th>Mentor</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {projectList.map((item) => (
                     <tr>
-                      <th>Project Name</th>
-                      <th>Status</th>
-                      <th>Start Time</th>
-                      <th>End Time</th>
-                      <th>Mentor</th>
+                      <td>{item.ProjectName}</td>
+                      <td>{item.Status}</td>
+                      <td>{item.StartTime}</td>
+                      <td>{item.EndTime.length > 0 ? item.EndTime : "-"}</td>
+                      <td>{item.Mentor}</td>
                     </tr>
-                  </thead>
-                  <tbody>{
-                    projectList.map(item=>
-                      <tr>
-                        <td>{item.ProjectName}</td>
-                        <td>{item.Status}</td>
-                        <td>{item.StartTime}</td>
-                        <td>{item.EndTime.length>0?item.EndTime:"-"}</td>
-                        <td>{item.Mentor}</td>
-                      </tr>
-                      )
-                    }
-                  </tbody>
-                </table>
-              </Card>
-            </div>
-            <div className='projectReportCard'>
-              <Card></Card>
-            </div>
-            
+                  ))}
+                </tbody>
+              </table>
+            </Card>
+          </div>
+          <div className="projectReportCard">
+            <Card headerName="Reports"></Card>
           </div>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
